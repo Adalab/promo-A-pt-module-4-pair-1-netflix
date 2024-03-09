bac17c56e858f208053f4cd4c7dd1fd894d10208
larafdezmorena <larafdezmorena@gmail.com>
2024-03-09 10:42:31 +0100

    .env

--- src/index.js
+++ src/index.js
@@ -1,61 +1,3 @@
-<<<<<<< HEAD
-const express = require('express');
-const cors = require('cors');
-
-// create and config server
-const server = express();
-server.use(cors());
-server.use(express.json());
-
-// init express aplication
-const serverPort = 4000;
-server.listen(serverPort, () => {
-  console.log(`Server listening at http://localhost:${serverPort}`);
-});
-
-//endpoints
-
-server.get('/movies', (req, res) => {
-  res.json({
-    success: true,
-    movies: [
-      {
-        id: '1',
-        title: 'Gambita de dama',
-        genre: 'Drama',
-        image:
-          '//beta.adalab.es/curso-intensivo-fullstack-recursos/apis/netflix-v1/images/gambito-de-dama.jpg',
-      },
-      {
-        id: '2',
-        title: 'Friends',
-        genre: 'Comedia',
-        image:
-          '//beta.adalab.es/curso-intensivo-fullstack-recursos/apis/netflix-v1/images/friends.jpg',
-      },
-    ],
-  });
-});
-
-//importar bibliotecas
-const mysql = require('mysql2/promise');
-
-async function getConnection() {
-  const connection = await mysql.createConnection({
-    host: '127.0.0.1',
-    database: 'Adalab-netflix',
-    user: 'root',
-    password: process.env.MYSQL_PASS,
-  });
-  await connection.connect();
-
-  console.log(
-    `Conexión establecida con la base de datos (identificador=${connection.threadId})`
-  );
-
-  return connection;
-}
-=======
 require('dotenv').config();
 
 const express = require('express');
@@ -114,4 +56,3 @@ async function getConnection() {
 
   return connection;
 }
->>>>>>> 416311e8d4a555eff0d9178872416439be89cc10
