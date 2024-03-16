a1e6da56b92ffb17e968707f8e82c76ea298d9da
larafdezmorena <larafdezmorena@gmail.com>
2024-03-16 11:25:23 +0100

    query params

--- web/src/services/api-movies.jsx
+++ web/src/services/api-movies.jsx
@@ -1,9 +1,9 @@
 // login
 
-const getMoviesFromApi = () => {
-  console.log(getMoviesFromApi);
+const getMoviesFromApi = (params) => {
+  console.log(params);
   // CAMBIA ESTE FETCH PARA QUE APUNTE A UN ENDPOINT DE TU SERVIDOR, PIENSA SI DEBE SER GET O POST, PIENSA QUÉ DATOS DEBES ENVIAR, ETC
-  return fetch('//localhost:4000/api/movies')
+  return fetch("//localhost:4000/api/movies?genre=comedia")
     .then((response) => response.json())
     .then((data) => {
       // CAMBIA EL CONTENIDO DE ESTE THEN PARA GESTIONAR LA RESPUESTA DEL SERVIDOR Y RETORNAR AL COMPONENTE APP LO QUE NECESITA
